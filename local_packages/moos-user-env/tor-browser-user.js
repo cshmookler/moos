4f6a998d239ee5c617d5dc4eb9ced9765cf868fd
Caden Shmookler <cshmookler@gmail.com>
2024-09-15 12:43:39 -0600

    Added the user environment.

---
+++ local_packages/moos-user-env/tor-browser-user.js
@@ -0,0 +1,7 @@
+user_pref("extensions.torlauncher.start_tor", false);
+user_pref("network.dns.disabled", false);
+user_pref("network.proxy.socks", " ");
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.calculator", true);
